be712ef9e02161f4d93577a17af95548c564375e
Catherina Balaa <catherina.balaa@hotmail.com>
2024-02-22 15:50:32 +0200

    Fixed routing in app routing module by using children and path leading to auth page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationPageComponent } from './modules/auth/pages/authentication-page/authentication-page.component';
+import { SigninComponent } from './modules/auth/components/signin/signin.component';
+import { SignupComponent } from './modules/auth/components/signup/signup.component';
 
 const routes: Routes = [
-  {path: 'auth',
-  loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
-  {path: 'news',
-  loadChildren: () => import('./modules/news-feed/news-feed.module').then(m => m.NewsFeedModule)},
-  {path: '', redirectTo: '/auth/signin',pathMatch: 'full'}
+  // {path: 'auth',
+  // loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
+  // {path: 'news',
+  // loadChildren: () => import('./modules/news-feed/news-feed.module').then(m => m.NewsFeedModule)},
+  // {path: '',
+  // loadChildren: () => import('./modules/auth/auth.module').then(m=>m.AuthModule)}
+  {path:'auth',component:AuthenticationPageComponent,
+  children:[
+    {path:'signin',component:SigninComponent},
+    {path:'signup',component:SignupComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './modules/auth/components/signin/signin.component';
 import { SignupComponent } from './modules/auth/components/signup/signup.component';
-import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
-import { NewsPageComponent } from './news-page/news-page.component';
+import { AuthenticationPageComponent } from './modules/auth/pages/authentication-page/authentication-page.component';
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     SignupComponent,
-    AuthenticationPageComponent,
-    NewsPageComponent
+    AuthenticationPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
