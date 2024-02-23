80f404e3eee0a81748990d384c0964c1f5b38951
Catherina Balaa <catherina.balaa@hotmail.com>
2024-02-23 10:42:56 +0200

    Added default route in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationPageComponent } from './modules/auth/pages/authentication-page/authentication-page.component';
+import { NewsPageComponent } from './modules/auth/pages/news-page/news-page.component';
 import { SigninComponent } from './modules/auth/components/signin/signin.component';
 import { SignupComponent } from './modules/auth/components/signup/signup.component';
 
@@ -15,7 +16,9 @@ const routes: Routes = [
   children:[
     {path:'signin',component:SigninComponent},
     {path:'signup',component:SignupComponent}
-  ]}
+  ]},
+  {path:'news',component:NewsPageComponent},
+  {path:'',redirectTo:'/auth/signin',pathMatch:'full'}
 ];
 
 @NgModule({
