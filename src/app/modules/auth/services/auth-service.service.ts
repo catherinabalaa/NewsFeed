871a29221d51ceb8913ef76df6edd998d0abab6f
Catherina Balaa <catherina.balaa@hotmail.com>
2024-03-18 13:30:58 +0200

    Deleted unwanted interceptor and cleaned code

--- src/app/modules/auth/services/auth-service.service.ts
+++ src/app/modules/auth/services/auth-service.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-// import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { SigninResponse } from '../models/signinResponse.model';
--- src/app/test.interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const testInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
