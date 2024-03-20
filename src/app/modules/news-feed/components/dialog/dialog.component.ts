7072f40f40518cc064a8e20381537952bc745fdf
Catherina Balaa <catherina.balaa@hotmail.com>
2024-03-20 12:13:33 +0200

    Added getlatestnews function in news service and modified news and dialog components in order to communicate with nytimes api endpoints

--- src/app/modules/news-feed/components/dialog/dialog.component.ts
+++ src/app/modules/news-feed/components/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class DialogComponent {
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any) {}
+
+  ngOnInit():void{};
 }
